883b69294bd00c68e383a4c5f12fa4d77d43098e
Magdiel Rotta Almeida <rotta.aero@gmail.com>
2023-05-04 22:19:44 -0300

    feat: footer component incompleto

--- meu-site/src/app/app.module.ts
+++ meu-site/src/app/app.module.ts
@@ -5,23 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './components/template/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatIconModule
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
